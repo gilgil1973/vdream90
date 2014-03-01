447282d05323553e52a06d105afe2a756aec820d
Gilbert Lee <admin@gilgil.net>
2014-03-01 15:07:04 +0900

    Add netcient and netserver

--- bin/_zip.bat
+++ bin/_zip.bat
@@ -1,1 +1,1 @@
-7z a vdream90bin(%date:~0,4%.%date:~5,2%.%date:~8,2%).zip getline.exe logserver.exe vlog.exe netc.exe nets.exe
+7z a vdream90bin(%date:~0,4%.%date:~5,2%.%date:~8,2%).zip getline.exe logserver.exe vlog.exe netc.exe netclient.exe nets.exe netserver.exe
--- bin/_zip.sh
+++ bin/_zip.sh
@@ -1,4 +1,4 @@
 NOW=$(date +"%Y.%m.%d")
 FILENAME="vdream90bin($NOW).zip"
-zip $FILENAME getline logserver vlog netc nets
+zip $FILENAME getline logserver vlog netc netclient nets netserver
 
